3ec3993d8b47b4af0d28b7c465ee5f9a518c3887
Shirinov Nurlan Anar <shirinov_n@itstep.edu.az>
2025-03-13 09:58:58 +0400

    some

---
+++ ManagementSystem.Domain/Entities/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace ManagementSystem.Domain.Entities;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string Model { get; set; }
+    public DateTime Year { get; set; }
+    public string Vendor { get; set; }
+    public decimal BasePrice { get; set; }
+}
